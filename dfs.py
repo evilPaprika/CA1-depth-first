03903775c0377574eb148b8ce87da18be9735498
evilPaprika <ivil.paprika@gmail.com>
2018-03-22 10:24:12 +0500

    убрал пробел

--- dfs.py
+++ dfs.py
@@ -4,7 +4,7 @@ from sortedcontainers import SortedSet
 def find(nodes):
     stack = [1]
     result = [SortedSet()]
-    while len(stack) > 0:
+    while stack:
         vertex = stack.pop() - 1
         for child in nodes[vertex]:
             if child not in result[len(result)-1]:
@@ -19,6 +19,7 @@ def find(nodes):
 
 
 if __name__ == '__main__':
-    components = find([[int(b) for b in a.split(" ")[:-1:]]
+    components = find([[int(b) for b in a.split()[:-1:]]
                        for a in open("in.txt").read().splitlines()[1::]])
+    # print("\n".join([''.join(str(i) + " " for i in s) + "0" for s in components]))
     open("out.txt", "w").write("\n".join([''.join(str(i) + " " for i in s) + "0" for s in components]))
